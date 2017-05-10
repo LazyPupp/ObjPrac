7df1d76c6e98c018effb29f7da8abbd91b96a27c
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-05-10 09:37:35 -0700

    o3p2

--- object3.js
+++ object3.js
@@ -1,4 +1,4 @@
-//Problem
+//Problem 1
 const loaf = {
   flour: 300,
   water: 210,
@@ -9,4 +9,25 @@ const loaf = {
 
 console.log(`${loaf.flour}, ${loaf.water}`);
 console.log(loaf.hydration());
+//Problem 2
+const myObj={
+	foo:"foo",
+	bar:"bar",
+	fum:100,
+	quux:"WHAT IS THIS WORD?",
+	spam: function(){
+		return [];
+	}
+}
+function loopy(obj){
+	for(let key in obj){
+		console.log(`${key}: ${obj[key]}\n`);
+	}
+}
+loopy(myObj);
+//Problem 3
+//Problem 4
+//Problem 5
+//Problem 6
+//Problem 7
 
