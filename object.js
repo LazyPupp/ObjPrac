a855944a3642691bad862facea2929b8eca9bad8
Jamie Wang <sephona@gmail.com>
2017-05-10 11:45:15 -0400

    Problem 4

--- object.js
+++ object.js
@@ -35,5 +35,19 @@ function personMaker() {
 }
 
 //Problem 4
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+
+  return obj;
+}
+
+
+var sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang'
+};
 
 
