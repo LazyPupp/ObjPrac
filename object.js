5724f7e0f8cc9be8019008a393a5f05e41b09df0
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-05-10 08:32:53 -0700

    object creator

---
+++ object.js
@@ -0,0 +1,15 @@
+function createMyObject() {
+  
+  const myObj={
+    foo: 'bar',
+    answerToUniverse:42,
+    'olly olly': 'oxen free',
+    sayHello: function(){
+      return 'hello';
+    }
+  }
+  return myObj;
+}
+
+
+
